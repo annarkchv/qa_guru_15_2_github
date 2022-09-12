e4d10cfb3b66142dd17b5222a046909638263695
AnnaRkchv <rakchaevaa@gmail.com>
2022-09-12 21:36:04 +0300

    Use random

--- src/test/java/guru/qa/ExampleTest.java
+++ src/test/java/guru/qa/ExampleTest.java
@@ -3,11 +3,13 @@ package guru.qa;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 public class ExampleTest {
 
     @Test
     void exampleTest1() {
-        Assertions.assertTrue(true);
+        Assertions.assertTrue(new Random().nextInt() > 1);
     }
 
     @Test
